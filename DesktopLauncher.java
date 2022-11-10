a15f3adb628d4ba238e72c0dd2e906b1f9307058
hereisaboringusername <71794563+hereisaboringusername@users.noreply.github.com>
2022-11-10 10:05:11 -0500

    Desktop

---
+++ DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.game.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Archery to Death";
+		config.width = 1000;
+		config.height = 444;
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
